8d9f3269dc6bbc59b3daa33e081574d4f7617f80
karinkarlssonroman <karinkarlssonroman@gmail.com>
2026-05-09 10:57:41 +0200

    create candidate accounts

--- JobWatch/JobWatch/Controllers/AccountController.cs
+++ JobWatch/JobWatch/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ namespace JobWatch.Controllers;
 public class AccountController : Controller
 {
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public AccountController(SignInManager<ApplicationUser> signInManager)
+    public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
     {
         _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     [HttpGet]
@@ -39,6 +41,19 @@ public class AccountController : Controller
             return Redirect(returnUrl);
         }
 
+        var user = await _userManager.FindByNameAsync(username);
+        if (user is not null && await _userManager.IsInRoleAsync(user, "Admin"))
+        {
+            var claims = await _userManager.GetClaimsAsync(user);
+            var department = claims.FirstOrDefault(c => c.Type == "Department")?.Value;
+
+            if (department == "Engineering")
+                return RedirectToAction("Index", "Engineering");
+
+            if (department == "Sales")
+                return RedirectToAction("Index", "Sales");
+        }
+
         return RedirectToAction("Index", "WhoAmI");
     }
 
@@ -54,5 +69,37 @@ public class AccountController : Controller
     public IActionResult AccessDenied()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateAccount()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateAccount(string username, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            ModelState.AddModelError(string.Empty, "Username and password are required.");
+            return View();
+        }
+
+        var user = new ApplicationUser { UserName = username };
+        var result = await _userManager.CreateAsync(user, password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+            return View();
+        }
+
+        await _userManager.AddToRoleAsync(user, "Candidate");
+        await _signInManager.SignInAsync(user, isPersistent: false);
+
+        return RedirectToAction("Index", "Jobs");
     }
 }
